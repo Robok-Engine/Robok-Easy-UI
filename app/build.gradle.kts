b9df31272284942432888d03c1a7438239bda3a4
trindadedev13 <devsuay@gmail.com>
2024-10-19 20:32:30 -0300

    fix: splashscreen attrs not foynd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies{
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.activity:activity-compose:1.9.3")
     
