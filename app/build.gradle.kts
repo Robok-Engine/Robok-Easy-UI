f1dbdc2a6f9861510389ca07136e5ecc5ff9044d
trindadedev13 <devsuay@gmail.com>
2024-10-19 20:52:47 -0300

    fix: signingConfig not are applied

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,7 @@ android {
         release {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release") 
         }
     }
 
