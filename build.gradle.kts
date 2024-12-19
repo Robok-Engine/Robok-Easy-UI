ec233608950997f1a94d647934388a6717dfcb9e
Aquiles Trindade <trindadedev13@gmail.com>
2024-12-19 14:10:27 -0300

    fix: also use version catalogs in build gradle root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.2" apply false
-    id("com.android.library") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0" apply false
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+  alias(libs.plugins.android.lib) apply false
+  alias(libs.plugins.android.app) apply false
+  alias(libs.plugins.kotlin) apply false
+  alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.kotlin.serialization) apply false
+  alias(libs.plugins.compose.compiler) apply false
 }
